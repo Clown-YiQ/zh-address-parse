5201ddee06c603dc42287c56245ec9bce893ca79
Clown-YiQ <58201075+Clown-YiQ@users.noreply.github.com>
2023-04-10 10:20:01 +0800

    Update address-parse.js

--- app/lib/address-parse.js
+++ app/lib/address-parse.js
@@ -486,8 +486,9 @@ const filterPhone = (address) => {
     address = address.replace(/(\d{3}) (\d{4}) (\d{4})/g, '$1$2$3')
     address = address.replace(/(\d{4}) \d{4} \d{4}/g, '$1$2$3')
     address = address.replace(/(\d{4})/g, '$1')
+    console.log(address, 'node')
 
-    const mobileReg = /(0|\+?86-?|17951|)?1[3456789]\d{9}(-\d{3,4})?/g
+    const mobileReg = /((\d{11}(\-\d{4}|\d{3})?)|^((\d{7,8})|(\d{4}|\d{3})-(\d{7,8})|(\d{4}|\d{3})-(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1})|(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1}))$)/g
     const mobile = mobileReg.exec(address)
     if (mobile) {
         phone = mobile[0]
